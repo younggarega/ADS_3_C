437df0f747281c5cf1862b0376c1bc5c7211d1ad
younggarega <younggarega@gmail.com>
2018-05-01 20:13:36 +0700

    update database.php

--- projectweb/authentication/app/database.php
+++ projectweb/authentication/app/database.php
@@ -5,14 +5,16 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
 
 $capsule->addConnection ([
-	'driver' => '',
-	'host' => '',
-	'database' => '',
-	'username' => '',
-	'password' => '',
-	'charset' => '',
-	'collation' => '',
-	'prefix' => '',
+	'driver' => $app->config->get('db.driver'),
+	'host' => $app->config->get('db.host'),
+	'database' => $app->config->get('db.name'),
+	'username' => $app->config->get('db.username'),
+	'password' => $app->config->get('db.password'),
+	'charset' => $app->config->get('db.charset'),
+	'collation' => $app->config->get('db.collation'),
+	'prefix' => $app->config->get('db.prefix'),
 ]);
+
+$capsule->bootEloquent();
 
 ?>
