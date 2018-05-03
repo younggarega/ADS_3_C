8a9795990f75d28c142fbd1beb7195fd5efddc10
younggarega <younggarega@gmail.com>
2018-05-03 23:19:05 +0700

    update start.php

--- projectweb/authentication/app/start.php
+++ projectweb/authentication/app/start.php
@@ -1,6 +1,8 @@
 <?php
 
 use Slim\Slim;
+use Slim\Views\Twig;
+
 use Noodlehaus\Config;
 
 use Codecourse\User\User;
@@ -15,7 +17,9 @@ define('INC_ROOT', dirname(__DIR__));
 require INC_ROOT . '/vendor/autoload.php';
 
 $app = new Slim([
-	'mode' => file_get_contents(INC_ROOT . '/mode.php')
+	'mode' => file_get_contents(INC_ROOT . '/mode.php'),
+	'view' => new Twig(),
+	'templates.path' => INC_ROOT . '/app/views'
 ]);
 
 $app->configureMode($app->config('mode'), function() use ($app) {
