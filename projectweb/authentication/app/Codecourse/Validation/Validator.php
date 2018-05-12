a64b32248f538f631e7f8102df44a3b493c691c8
younggarega <younggarega@gmail.com>
2018-05-12 13:59:37 +0700

    update register.php

--- projectweb/authentication/app/Codecourse/Validation/Validator.php
+++ projectweb/authentication/app/Codecourse/Validation/Validator.php
@@ -3,10 +3,36 @@
 namespace Codecourse\Validation;
 
 use Violin\Violin;
+use Codecourse\User\User;
 
 class Validator extends Violin
 {
+	protected $user;
 
+	public function __construct(User $user)
+	{
+		$this->user = $user;
+
+		$this->addFieldMessages([
+			'email' => [
+				'uniqueEmail' => 'That email is already in use.'
+			],
+			'username' => [
+				'uniqueUsername' => 'That username is already in use.']
+		]);
+	}
+
+	public function validate_uniqueEmail($value, $input, $args)
+	{
+		$user = $this->user->where('email', $value);
+
+		 return ! (bool) $user->count();
+	}
+
+	public function validate_uniqueUsername($value, $input, $args)
+	{
+		return ! (bool) $this->user->where('username', $value)->count();  
+	}
 }
 
 ?>
--- projectweb/authentication/app/views/templates/partials/navigation.php
+++ projectweb/authentication/app/views/templates/partials/navigation.php
@@ -1,4 +1,8 @@
 <ul>
 	<li><a href="{{ urlFor('home') }}">Home</a></li>
+	<li><a href="{{ urlFor('register') }}">Notification</a></li>
+	<li><a href="{{ urlFor('register') }}">Setting</a></li>
+	<li><a href="{{ urlFor('register') }}">History</a></li>
+	<li><a href="{{ urlFor('register') }}">Help</a></li>
 	<li><a href="{{ urlFor('register') }}">Register</a></li>
 </ul>
